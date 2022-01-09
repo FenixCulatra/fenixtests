ebf579f86191483bbe9d06a8561c7a74e98eec25
FenixCulatra <91470763+FenixCulatra@users.noreply.github.com>
2022-01-09 09:57:39 -0300

    Create botaotamanho.js

---
+++ botaotamanho.js
@@ -0,0 +1,62 @@
+var tipo = "h1"
+var p = document.querySelectorAll(tipo)
+tamanho = 30
+if (p.length == 0) {
+	var tipo = "h2"
+	var p = document.querySelectorAll(tipo)
+	tamanho = 24
+}
+if (p.length == 0) {
+	var tipo = "h3"
+	var p = document.querySelectorAll(tipo)
+	tamanho = 20
+}
+if (p.length == 0) {
+	var tipo = "h4"
+	var p = document.querySelectorAll(tipo)
+	tamanho = 16
+}
+if (p.length == 0) {
+	var tipo = "h5"
+	var p = document.querySelectorAll(tipo)
+	tamanho = 12
+}
+if (p.length == 0) {
+	var tipo = "h6"
+	var p = document.querySelectorAll(tipo)
+	tamanho = 8
+}
+//-----------------------------------------------------
+for (var x = 0; x < p.length; x++){
+	p[x].style.fontSize = `${tamanho}px`
+	espaco = document.creatElement("p")
+	var butao = document.createElement("button")
+	p[x].appendChild(espaco)
+	p[x].appendChild(butao)
+	espaco.textContent = " "
+	butao.setAttribute("onclick","javascript: aumenta(this.value)")
+	butao.setAttribute("value",`${x}`)
+	butao.setAttribute("style","background-color: black; color: white; border-radius: 10px;")
+	butao.textContent = "+"
+	var butao = document.createElement("button")
+	p[x].appendChild(espaco)
+	p[x].appendChild(butao)
+	butao.setAttribute("onclick","javascript: diminui(this.value)")
+	butao.setAttribute("value",`${x}`)
+	butao.setAttribute("style","background-color: black; color: white; border-radius: 10px;")
+	espaco.textContent = " "
+	butao.textContent = "-"
+	p[x].value = tamanho
+}
+botao = document.querySelectorAll("button")
+function aumenta(quem){
+	tam = p[quem].value + 5
+	p[quem].style.fontSize = `${tam}px`
+	p[quem].value = tam
+}
+function diminui(quem){
+	if (p[quem].value > 25){
+	tam = p[quem].value - 5
+	p[quem].style.fontSize = `${tam}px`
+	p[quem].value = tam
+}}
